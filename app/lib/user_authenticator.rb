b1c13c2d756e80c5475af691a2b6c2e3705cda99
Hitsuji-otoko <shmahoro@gmail.com>
2019-11-17 16:11:32 +0900

    OAuth: refactoring-for-sucess-test

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -1,5 +1,26 @@
 class UserAuthenticator
-    def initialize(code)
+    class AuthenticationError < StandardError; end
     
+    attr_reader :user
+
+    def initialize(code)
+        @code = code
     end
+
+    def perform
+    # performメソッドがユーザからどのように見えるか
+        client = Octokit::client.new(
+            client_id: ENV['GITHUB_CLIENT_ID'],
+            client_password: ENV['GITHUB_CLIENT_SECRET']
+        )
+        res = client.exchange_code_for_token(code)
+        if res.error.present?
+            raise AuthenticationError
+        else
+        end
+    end
+
+    private
+
+    attr_reader :code
 end
