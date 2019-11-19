131dc085d0f534cf13736e31bf90696822f14f69
Hitsuji-otoko <shmahoro@gmail.com>
2019-11-19 20:58:26 +0900

    Create-Endpoint

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -3,6 +3,7 @@ class AccessTokensController < ApplicationController
     # 認証の際、不適切なデータ形式でトークン送った場合にエラーを返す
     authenticator = User.Authenticator.new(params[:code])
     if authenticator.perform
+      
     else error = {
         "status"=> "401",
         "source"=> { "pointer" => "/code" },
