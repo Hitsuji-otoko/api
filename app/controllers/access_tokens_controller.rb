cb695597c6791b839e2b80ee967c783f9ef9819a
Hitsuji-otoko <shmahoro@gmail.com>
2019-11-19 21:10:03 +0900

    define-JSONerror-as-application

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -1,23 +1,9 @@
 class AccessTokensController < ApplicationController
+
   def create
     # 認証の際、不適切なデータ形式でトークン送った場合にエラーを返す
-    authenticator = User.Authenticator.new(params[:code])
-    begin
-      authenticator.perform
-    rescue UserAuthenticator::AuthenticationError 
-    end
-  end
-
-  private
-
-  def authencation_error
-    error = {
-      "status"=> "401",
-      "source"=> { "pointer" => "/code" },
-      "title"=>  "Authentication codeis invalid",
-      "detail"=> "You must provide valid code in order to exchange it for token."
-    }
-    render json: { "errors": [error] }, status: 401
+    authenticator = UserAuthenticator.new(params[:code])
+    authenticator.perform
   end
   
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+    # 処理するエラークラスを第一引数として渡す
+    rescue_from UserAuthenticator::AuthenticationError, with: :authencation_error
+
+    private
+
+    def authencation_error
+      error = {
+        "status"=> "401",
+        "source"=> { "pointer" => "/code" },
+        "title"=>  "Authentication codeis invalid",
+        "detail"=> "You must provide valid code in order to exchange it for token."
+      }
+      render json: { "errors": [error] }, status: 401
+    end
 end
