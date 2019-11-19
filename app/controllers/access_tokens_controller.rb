eedc045a26ceee960990c2827400f95d1f886131
Hitsuji-otoko <shmahoro@gmail.com>
2019-11-19 21:03:35 +0900

    refactoring-Endpoint

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -2,15 +2,22 @@ class AccessTokensController < ApplicationController
   def create
     # 認証の際、不適切なデータ形式でトークン送った場合にエラーを返す
     authenticator = User.Authenticator.new(params[:code])
-    if authenticator.perform
-      
-    else error = {
-        "status"=> "401",
-        "source"=> { "pointer" => "/code" },
-        "title"=>  "Authentication codeis invalid",
-        "detail"=> "You must provide valid code in order to exchange it for token."
-      }
-      render json: { "errors": [error] }, status: 401
+    begin
+      authenticator.perform
+    rescue UserAuthenticator::AuthenticationError 
     end
   end
+
+  private
+
+  def authencation_error
+    error = {
+      "status"=> "401",
+      "source"=> { "pointer" => "/code" },
+      "title"=>  "Authentication codeis invalid",
+      "detail"=> "You must provide valid code in order to exchange it for token."
+    }
+    render json: { "errors": [error] }, status: 401
+  end
+  
 end
